d48c2bb744ad22c89c81cddd973ec38ad238fe1d
Velcatt <antoinelacroix68@gmail.com>
2025-09-12 14:55:09 +0200

    challenge day05 opti

--- day05/Task2.Chall.py
+++ day05/Task2.Chall.py
@@ -1,7 +1,18 @@
 import random
 
+
+def fast_sort(liste):
+    if len(liste) <= 1:
+        return liste
+    else:
+        pivot = liste[1]
+        greater = [n for n in liste[1:] if n > pivot]
+        lesser = [n for n in liste[1:] if n <= pivot]
+        return fast_sort(greater) + [pivot] + fast_sort(lesser)
+
+
 liste = []
-for i in range(1000001):
+for i in range(100001):
     liste.append(random.randint(0, 10000000000))
-liste.sort()
-print(liste)
+
+print(fast_sort(liste)[::-1])
