f04e9e2e45095fd54f5ebe742a0dab6c76308d8c
Velcatt <antoinelacroix68@gmail.com>
2025-09-12 16:10:36 +0200

    quick sort pour le challenge day05

--- day05/Task2.Chall.py
+++ day05/Task2.Chall.py
@@ -2,14 +2,14 @@ import random
 import time
 
 
-def fast_sort(liste):
+def quick_sort(liste):
     if len(liste) <= 1:
         return liste
     else:
         pivot = liste[0]
         greater = [n for n in liste[1:] if n > pivot]
         lesser = [n for n in liste[1:] if n <= pivot]
-        return fast_sort(greater) + [pivot] + fast_sort(lesser)
+        return quick_sort(greater) + [pivot] + quick_sort(lesser)
 
 
 start = time.time()
@@ -17,6 +17,6 @@ liste = []
 for i in range(100001):
     liste.append(random.randint(0, 10000000000))
 
-print(fast_sort(liste)[::-1])
+print(quick_sort(liste)[::-1])
 
 print(time.time() - start)
